0fd3e6459b27fd8d4303e3141c19462d22004572
Yi Hsiao <hsiaoyi0504@gmail.com>
2018-09-08 20:45:01 -0400

    add data of food api

--- build.py
+++ build.py
@@ -20,3 +20,24 @@ for i, report in enumerate(data['results']):
 
 with open('./server/data.pkl', 'wb') as out_f:
     pickle.dump([id_report_mapping,  word_report_ids_mapping], out_f)
+
+
+with open('./data/food.json') as g:
+    all_strings2 = g.read()
+    data2 = json.loads(all_strings2)
+
+num_of_reports2 = len(data2['results2'])
+id_report_mapping2 = dict(zip(range(num_of_reports2), data2['results2']))
+# generate dictonary mapping word (should be a food) to report id
+word_report_ids_mapping2 = {}
+for j, report2 in enumerate(data2['results2']):
+    s_words = report2['name'].lower().split(' ')
+    for s in s_words:
+        if s in word_report_ids_mapping2:
+            word_report_ids_mapping2[s].append(j)
+        else:
+            word_report_ids_mapping2[s] = [j]
+
+
+with open('./server/data2.pkl', 'wb') as out_g:
+    pickle.dump([id_report_mapping2,  word_report_ids_mapping2], out_g)
--- server/main.py
+++ server/main.py
@@ -8,6 +8,8 @@ app = Flask(__name__)
 with open(join(here, 'data.pkl'), 'rb') as f:
     id_report_mapping,  word_report_ids_mapping = pickle.load(f)
 
+with open(join(here, 'data2.pkl'), 'rb') as g:
+    id_report_mapping2,  word_report_ids_mapping2 = pickle.load(g)
 
 @app.route('/food/<food_name>')
 def get_food_info(food_name):
@@ -20,16 +22,47 @@ def get_food_info(food_name):
         num_class_1 = report_classes.count('Class I')
         num_class_2 = report_classes.count('Class II')
         num_class_3 = report_classes.count('Class III')
+        ratings_recall = 100 - num_class_3 - 0.6 * num_class_2 - 0.3 * num_class_1
+
+        if food_name in word_report_ids_mapping2:
+            ids2 = word_report_ids_mapping2[food_name]
+            protein = []
+            sugars = []
+            carbohydrate = []
+            fat = []
+            for j in ids2:
+                if 'nutrition-per-100g' in id_report_mapping2[j]:
+                    key = 'nutrition-per-100g'
+                else:
+                    key = 'nutrition-per-100ml'
+                energy = id_report_mapping2[j][key]['energy']
+                protein = id_report_mapping2[j][key]['protein']
+                sugars = id_report_mapping2[j][key]['sugars']
+                carbohydrate = id_report_mapping2[j][key]['carbohydrate']
+                fat = id_report_mapping2[j][key]['fat']
+                trans_fat = id_report_mapping2[j][key]['trans-fat']
+                if trans_fat >= 1:
+                    much_trans_fat = True
+                else:
+                    much_trans_fat = False
+                nutrition_ratings = 100 - 0.05*energy + 1 * protein - 0.5 * sugars - 0.05 * fat - 0.1*trans_fat
 
         return jsonify({
             'num_of_report': num_of_report,
-            'overall_score': '5',
             'food_name': food_name,
             'num_class_1': num_class_1,
             'num_class_2': num_class_2,
             'num_class_3': num_class_3,
+            'return_ratings': ratings_recall,
+            'energy': energy,
+            'protein': protein,
+            'sugars': sugars,
+            'carbohydrate': carbohydrate,
+            'fat': fat,
+            'much_trans_fat': much_trans_fat,
+            'nutrition_ratings': nutrition_ratings
         })
     else:
         return jsonify({
-            'food_name': food_name
+            'food_name': food_name,
         })
